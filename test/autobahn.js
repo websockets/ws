552e1d7b45d2f4122400a95b2d60ed13151dcfcc
einaros <einaros@gmail.com>
2011-11-24 22:16:19 +0100

    added report generation every 10 tests

--- test/autobahn.js
+++ test/autobahn.js
@@ -2,8 +2,12 @@ var WebSocket = require('../');
 var currentTest = 1;
 var testCount = null;
 
-function nextTest() {
+function nextTest(skipReports) {
     if (currentTest > testCount) return;
+    if (!skipReports && currentTest % 10 == 0) {
+        updateReports();
+        return;
+    }
     console.log('Running test case #' + currentTest);
     var ws = new WebSocket('ws://localhost:9001/runCase?case=' + currentTest + '&agent=easy-websocket');
     ws.on('message', function(data, flags) {
@@ -12,6 +16,13 @@ function nextTest() {
     ws.on('close', function() {
         currentTest += 1;
         process.nextTick(nextTest);
+    });
+}
+
+function updateReports() {
+    var ws = new WebSocket('ws://localhost:9001/updateReports?agent=easy-websocket');
+    ws.on('close', function() {
+        process.nextTick(nextTest.bind(this, true));
     });
 }
 
