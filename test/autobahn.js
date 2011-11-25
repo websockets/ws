e846e42a179c7ef9bf5b9144601c1fcbdaf7bae3
einaros <einaros@gmail.com>
2011-11-25 09:05:52 +0100

    unstable push - still adapting to autobahn tests. text and binary fixed so far.

--- test/autobahn.js
+++ test/autobahn.js
@@ -1,19 +1,20 @@
 var WebSocket = require('../');
-var currentTest = 1;
+var currentTest = 17;
 var testCount = null;
 
 function nextTest(skipReports) {
     if (currentTest > testCount) return;
-    if (!skipReports && currentTest % 10 == 0) {
+    if (!skipReports && currentTest % 2 == 0) {
         updateReports();
         return;
     }
-    console.log('Running test case #' + currentTest);
+    console.log('== Running test case #' + currentTest + ' ==');
     var ws = new WebSocket('ws://localhost:9001/runCase?case=' + currentTest + '&agent=easy-websocket');
     ws.on('message', function(data, flags) {
-        ws.send(data, {binary: flags.binary === true, mask: flags.masked === true});
+        // console.log(arguments, {binary: flags.binary === true, mask: flags.masked === true});
+        ws.send(data, {binary: flags.binary === true, mask: true});
     });
-    ws.on('close', function() {
+    ws.on('close', function(data) {
         currentTest += 1;
         process.nextTick(nextTest);
     });
