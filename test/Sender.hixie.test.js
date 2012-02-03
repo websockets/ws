d1173e0bcfb5df0e35a1d6d86543d8e664f6279e
einaros <einaros@gmail.com>
2012-02-03 20:48:44 +0100

    fixed hixie header issues

--- test/Sender.hixie.test.js
+++ test/Sender.hixie.test.js
@@ -3,7 +3,7 @@ var assert = require('assert')
 require('should');
 require('./hybi-common');
 
-describe('Sender', function() {  
+describe('Sender', function() {
   it('frames and sends a text message', function(done) {
     var message = 'Hello world';
     var received;
@@ -17,6 +17,19 @@ describe('Sender', function() {
     sender.send(message, {}, function() {
       received.toString('utf8').should.eql('\u0000' + message + '\ufffd');
       done();
-    })
+    });
+  });
+
+  it('throws an exception for binary data', function(done) {
+    var socket = {
+      write: function(data, encoding, cb) {
+        process.nextTick(cb);
+      }
+    };
+    var sender = new Sender(socket, {});
+    sender.on('error', function() {
+      done();
+    });
+    sender.send(new Buffer(100), {binary: true}, function() {});
   });
 });
