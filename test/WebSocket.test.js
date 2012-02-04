2667bcccfe50f3a70b76c05039ebefd9413ee19c
einaros <einaros@gmail.com>
2012-02-04 11:50:47 +0100

    restructured test cases

--- test/WebSocket.test.js
+++ test/WebSocket.test.js
@@ -148,49 +148,51 @@ describe('WebSocket', function() {
     });
   });
 
-  it('can disconnect before connection is established', function(done) {server.createServer(++port, function(srv) {
-      var ws = new WebSocket('ws://localhost:' + port);
-      ws.terminate();
-      ws.on('open', function() {
-        assert.fail('connect shouldnt be raised here');
-      });
-      ws.on('close', function() {
-        srv.close();
-        done();
-      });
-    });
-  });
-
-  it('can close before connection is established', function(done) {
-    server.createServer(++port, function(srv) {
-      var ws = new WebSocket('ws://localhost:' + port);
-      ws.close(1001);
-      ws.on('open', function() {
-        assert.fail('connect shouldnt be raised here');
-      });
-      ws.on('close', function() {
-        srv.close();
-        done();
+  describe('connection establishing', function() {
+    it('can disconnect before connection is established', function(done) {server.createServer(++port, function(srv) {
+        var ws = new WebSocket('ws://localhost:' + port);
+        ws.terminate();
+        ws.on('open', function() {
+          assert.fail('connect shouldnt be raised here');
+        });
+        ws.on('close', function() {
+          srv.close();
+          done();
+        });
       });
     });
-  });
 
-  it('invalid server key is denied', function(done) {
-    server.createServer(++port, server.handlers.invalidKey, function(srv) {
-      var ws = new WebSocket('ws://localhost:' + port);
-      ws.on('error', function() {
-        srv.close();
-        done();
+    it('can close before connection is established', function(done) {
+      server.createServer(++port, function(srv) {
+        var ws = new WebSocket('ws://localhost:' + port);
+        ws.close(1001);
+        ws.on('open', function() {
+          assert.fail('connect shouldnt be raised here');
+        });
+        ws.on('close', function() {
+          srv.close();
+          done();
+        });
       });
     });
-  });
 
-  it('close event is raised when server closes connection', function(done) {
-    server.createServer(++port, server.handlers.closeAfterConnect, function(srv) {
-      var ws = new WebSocket('ws://localhost:' + port);
-      ws.on('close', function() {
-        srv.close();
-        done();
+    it('invalid server key is denied', function(done) {
+      server.createServer(++port, server.handlers.invalidKey, function(srv) {
+        var ws = new WebSocket('ws://localhost:' + port);
+        ws.on('error', function() {
+          srv.close();
+          done();
+        });
+      });
+    });
+
+    it('close event is raised when server closes connection', function(done) {
+      server.createServer(++port, server.handlers.closeAfterConnect, function(srv) {
+        var ws = new WebSocket('ws://localhost:' + port);
+        ws.on('close', function() {
+          srv.close();
+          done();
+        });
       });
     });
   });
