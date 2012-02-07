eed413f685893f838041b07a68e35800c508b69d
einaros <einaros@gmail.com>
2012-02-07 10:28:49 +0100

    added browser-based example

---
+++ examples/serverstats/server.js
@@ -0,0 +1,19 @@
+var WebSocketServer = require('../../').Server
+  , express = require('express')
+  , app = express.createServer();
+
+app.use(express.static(__dirname + '/public'));
+
+var wss = new WebSocketServer({server: app});
+wss.on('connection', function(ws) {
+  var id = setInterval(function() {
+    ws.send(JSON.stringify(process.memoryUsage()));
+  }, 100);
+  console.log('started client interval');
+  ws.on('close', function() {
+    console.log('stopping client interval');
+    clearInterval(id);
+  })
+});
+
+app.listen(8080);
