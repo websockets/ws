c9606f90a6ef468a1854917e242ec8640df03d98
einaros <einaros@gmail.com>
2012-03-08 11:03:10 +0100

    add missing sender methods to hixie sender. fixes #37

--- test/Sender.hixie.test.js
+++ test/Sender.hixie.test.js
@@ -4,32 +4,51 @@ require('should');
 require('./hybi-common');
 
 describe('Sender', function() {
-  it('frames and sends a text message', function(done) {
-    var message = 'Hello world';
-    var received;
-    var socket = {
-      write: function(data, encoding, cb) {
-        received = data;
-        process.nextTick(cb);
-      }
-    };
-    var sender = new Sender(socket, {});
-    sender.send(message, {}, function() {
-      received.toString('utf8').should.eql('\u0000' + message + '\ufffd');
-      done();
+  describe('#send', function() {
+    it('frames and sends a text message', function(done) {
+      var message = 'Hello world';
+      var received;
+      var socket = {
+        write: function(data, encoding, cb) {
+          received = data;
+          process.nextTick(cb);
+        }
+      };
+      var sender = new Sender(socket, {});
+      sender.send(message, {}, function() {
+        received.toString('utf8').should.eql('\u0000' + message + '\ufffd');
+        done();
+      });
+    });
+
+    it('throws an exception for binary data', function(done) {
+      var socket = {
+        write: function(data, encoding, cb) {
+          process.nextTick(cb);
+        }
+      };
+      var sender = new Sender(socket, {});
+      sender.on('error', function() {
+        done();
+      });
+      sender.send(new Buffer(100), {binary: true}, function() {});
     });
   });
 
-  it('throws an exception for binary data', function(done) {
-    var socket = {
-      write: function(data, encoding, cb) {
-        process.nextTick(cb);
-      }
-    };
-    var sender = new Sender(socket, {});
-    sender.on('error', function() {
-      done();
+  describe('#close', function() {
+    it('sends a hixie close frame', function(done) {
+      var received;
+      var socket = {
+        write: function(data, encoding, cb) {
+          received = data;
+          process.nextTick(cb);
+        }
+      };
+      var sender = new Sender(socket, {});
+      sender.close(null, null, null, function() {
+        received.toString('utf8').should.eql('\u0000\ufffd');
+        done();
+      });
     });
-    sender.send(new Buffer(100), {binary: true}, function() {});
   });
 });
