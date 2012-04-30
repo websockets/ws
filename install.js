43f493a27103e70f3d665a7aaac57be86427b457
einaros <einaros@gmail.com>
2012-05-01 01:19:17 +0200

    add install script which makes native extension optional

---
+++ install.js
@@ -0,0 +1,29 @@
+var spawn = require('child_process').spawn
+  , exec = require('child_process').exec;
+
+var build_native = process.env['npm_package_config_native'] != null ? process.env['npm_package_config_native'] : 'false';
+build_native = build_native == 'true' ? true : false;
+if(build_native) {
+  process.stdout.write("================================================================================\n");
+  process.stdout.write("=                                                                              =\n");
+  process.stdout.write("=  Building WS with blazing fast native extensions.                            =\n");
+  process.stdout.write("=                                                                              =\n");
+  process.stdout.write("================================================================================\n");
+
+  var gyp = spawn('node-gyp', ['rebuild']);
+  gyp.stdout.on('data', function(data) {
+    process.stdout.write(data);
+  });
+  gyp.stderr.on('data', function(data) {
+    process.stdout.write(data);
+  });
+  gyp.on('exit', function(code) {});
+}
+else {
+  process.stdout.write("================================================================================\n");
+  process.stdout.write("=                                                                              =\n");
+  process.stdout.write("=  To install WS with blazing fast native extensions, use                      =\n");
+  process.stdout.write("=       <npm install ws --ws:native>                                           =\n");
+  process.stdout.write("=                                                                              =\n");
+  process.stdout.write("================================================================================\n");
+}
