d65468c848dfc9ca587226e2027b2bed8191e0f0
einaros <einaros@gmail.com>
2012-12-06 09:39:51 +0100

    further changes to ensure that install runs without native support

--- install.js
+++ install.js
@@ -1,5 +1,4 @@
-var spawn = require('child_process').spawn
-  , exec = require('child_process').exec
+var exec = require('child_process').exec
   , tinycolor = require('tinycolor')
   , fs = require('fs')
   , version = JSON.parse(fs.readFileSync(__dirname + '/package.json', 'utf8')).version
@@ -7,23 +6,31 @@ var spawn = require('child_process').spawn
 
 console.log('[ws v%s]'.blue + ' Attempting to compile blazing fast native extensions.'.green, version);
 
-var gyp = exec('node-gyp rebuild', {cwd: __dirname});
-gyp.stdout.on('data', function(data) {
-  if (verbose) process.stdout.write(data);
-});
-gyp.stderr.on('data', function(data) {
-  if (verbose) process.stdout.write(data);
-});
-gyp.on('exit', function(code) {
-  if (code !== 0) {
-    console.log('[ws v%s]'.blue + ' Native code compile failed (but the module will still work):'.yellow, version);
-    console.log('[ws v%s]'.blue + ' The native extensions are faster, but not required.'.yellow, version);
-    console.log('[ws v%s]'.blue + ' On Windows, native extensions require Visual Studio and Python.'.yellow, version);
-    console.log('[ws v%s]'.blue + ' On Unix, native extensions require Python, make and a C++ compiler.'.yellow, version);
-    console.log('[ws v%s]'.blue + ' Start npm with --ws:verbose to show compilation output (if any).'.yellow, version);
-  }
-  else {
-    console.log('[ws v%s]'.blue + ' Native extension compilation successful!'.green, version);
- }
+function exitWithWarning() {
+  console.log('[ws v%s]'.blue + ' Native code compile failed (but the module will still work):'.yellow, version);
+  console.log('[ws v%s]'.blue + ' The native extensions are faster, but not required.'.yellow, version);
+  console.log('[ws v%s]'.blue + ' On Windows, native extensions require Visual Studio and Python.'.yellow, version);
+  console.log('[ws v%s]'.blue + ' On Unix, native extensions require Python, make and a C++ compiler.'.yellow, version);
+  console.log('[ws v%s]'.blue + ' Start npm with --ws:verbose to show compilation output (if any).'.yellow, version);
   process.exit();
-});
+}
+
+try {
+  var gyp = exec('node-gyp rebuild', {cwd: __dirname});
+  gyp.stdout.on('data', function(data) {
+    if (verbose) process.stdout.write(data);
+  });
+  gyp.stderr.on('data', function(data) {
+    if (verbose) process.stdout.write(data);
+  });
+  gyp.on('exit', function(code) {
+    if (code !== 0) exitWithWarning();
+    else {
+      console.log('[ws v%s]'.blue + ' Native extension compilation successful!'.green, version);
+   }
+    process.exit();
+  });
+}
+catch (e) {
+  exitWithWarning();
+}
