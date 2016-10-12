3c8acc1f78bff777852e4ab57a1be689b1fc480d
Luigi Pinca <luigipinca@gmail.com>
2016-10-12 16:42:50 +0200

    [benchmark] Move functions only used by the worker in the worker branch

--- bench/speed.js
+++ bench/speed.js
@@ -6,59 +6,6 @@ const WebSocket = require('../');
 
 const port = 8181;
 
-function roundPrec (num, prec) {
-  const mul = Math.pow(10, prec);
-  return Math.round(num * mul) / mul;
-}
-
-function humanSize (bytes) {
-  if (bytes >= 1048576) return roundPrec(bytes / 1048576, 2) + ' MiB';
-  if (bytes >= 1024) return roundPrec(bytes / 1024, 2) + ' KiB';
-  return roundPrec(bytes, 2) + ' B';
-}
-
-function generateRandomData (size) {
-  const buffer = Buffer.alloc(size);
-  for (var i = 0; i < size; ++i) {
-    buffer[i] = ~~(Math.random() * 127);
-  }
-  return buffer;
-}
-
-function runConfig (useBinary, roundtrips, size, randomBytes, cb) {
-  const data = randomBytes.slice(0, size);
-  const client = new WebSocket(`ws://localhost:${port}`);
-  var roundtrip = 0;
-  var time;
-
-  client.on('error', (err) => {
-    console.error(err.stack);
-    cluster.worker.kill();
-  });
-  client.on('open', () => {
-    time = process.hrtime();
-    client.send(data, { binary: useBinary });
-  });
-  client.on('message', () => {
-    if (++roundtrip !== roundtrips) return client.send(data, { binary: useBinary });
-
-    var elapsed = process.hrtime(time);
-    elapsed = elapsed[0] * 1e9 + elapsed[1];
-
-    console.log(
-      '%d roundtrips of %s %s data:\t%ss\t%s',
-      roundtrips,
-      humanSize(size),
-      useBinary ? 'binary' : 'text',
-      roundPrec(elapsed / 1e9, 1),
-      humanSize(size * roundtrips / elapsed * 1e9) + '/s'
-    );
-
-    client.close();
-    cb();
-  });
-}
-
 if (cluster.isMaster) {
   const wss = new WebSocket.Server({
     maxPayload: 600 * 1024 * 1024,
@@ -85,14 +32,63 @@ if (cluster.isMaster) {
     [false, 100, 1024 * 1024]
   ];
 
+  const roundPrec = (num, prec) => {
+    const mul = Math.pow(10, prec);
+    return Math.round(num * mul) / mul;
+  };
+
+  const humanSize = (bytes) => {
+    if (bytes >= 1048576) return roundPrec(bytes / 1048576, 2) + ' MiB';
+    if (bytes >= 1024) return roundPrec(bytes / 1024, 2) + ' KiB';
+    return roundPrec(bytes, 2) + ' B';
+  };
+
   const largest = configs.reduce((prev, curr) => curr[2] > prev ? curr[2] : prev, 0);
   console.log('Generating %s of test data...', humanSize(largest));
-  const randomBytes = generateRandomData(largest);
+  const randomBytes = Buffer.allocUnsafe(largest);
+
+  for (var i = 0; i < largest; ++i) {
+    randomBytes[i] = ~~(Math.random() * 127);
+  }
+
+  const runConfig = (useBinary, roundtrips, size, cb) => {
+    const data = randomBytes.slice(0, size);
+    const ws = new WebSocket(`ws://localhost:${port}`);
+    var roundtrip = 0;
+    var time;
+
+    ws.on('error', (err) => {
+      console.error(err.stack);
+      cluster.worker.kill();
+    });
+    ws.on('open', () => {
+      time = process.hrtime();
+      ws.send(data, { binary: useBinary });
+    });
+    ws.on('message', () => {
+      if (++roundtrip !== roundtrips) return ws.send(data, { binary: useBinary });
+
+      var elapsed = process.hrtime(time);
+      elapsed = elapsed[0] * 1e9 + elapsed[1];
+
+      console.log(
+        '%d roundtrips of %s %s data:\t%ss\t%s',
+        roundtrips,
+        humanSize(size),
+        useBinary ? 'binary' : 'text',
+        roundPrec(elapsed / 1e9, 1),
+        humanSize(size * roundtrips / elapsed * 1e9) + '/s'
+      );
+
+      ws.close();
+      cb();
+    });
+  };
 
   (function run () {
     if (configs.length === 0) return cluster.worker.kill();
     var config = configs.shift();
-    config.push(randomBytes, run);
+    config.push(run);
     runConfig.apply(null, config);
   })();
 }
