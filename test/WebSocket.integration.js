31c05b01cc57d4e688b7b79a59ef22931c5b0b42
Luigi Pinca <luigipinca@gmail.com>
2016-10-28 12:21:00 +0200

    [test] Add secure WebSocket (wss) integration test

--- test/WebSocket.integration.js
+++ test/WebSocket.integration.js
@@ -5,16 +5,37 @@ const assert = require('assert');
 const WebSocket = require('..');
 
 describe('WebSocket', function () {
-  it('communicates successfully with echo service', function (done) {
+  it('communicates successfully with echo service (ws)', function (done) {
     const ws = new WebSocket('ws://echo.websocket.org/', {
-      origin: 'http://websocket.org',
+      origin: 'ws://echo.websocket.org',
       protocolVersion: 13
     });
     const str = Date.now().toString();
 
     let dataReceived = false;
 
-    ws.on('open', () => ws.send(str, { mask: true }));
+    ws.on('open', () => ws.send(str));
+    ws.on('close', () => {
+      assert.ok(dataReceived);
+      done();
+    });
+    ws.on('message', (data) => {
+      dataReceived = true;
+      assert.strictEqual(data, str);
+      ws.close();
+    });
+  });
+
+  it('communicates successfully with echo service (wss)', function (done) {
+    const ws = new WebSocket('wss://echo.websocket.org/', {
+      origin: 'wss://echo.websocket.org',
+      protocolVersion: 13
+    });
+    const str = Date.now().toString();
+
+    let dataReceived = false;
+
+    ws.on('open', () => ws.send(str));
     ws.on('close', () => {
       assert.ok(dataReceived);
       done();
