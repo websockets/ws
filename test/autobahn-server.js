3b327c97e8efeb01b96f0646119eacc0791e3b8d
Antti Risteli <antti@kiinnost.us>
2016-11-15 22:54:37 +0200

    Clean up Autobahn tests

--- test/autobahn-server.js
+++ test/autobahn-server.js
@@ -1,31 +1,12 @@
 'use strict';
 
-var WebSocket = require('../');
-var WebSocketServer = WebSocket.Server;
+const WebSocket = require('../');
 
-process.on('uncaughtException', function (err) {
-  console.log('Caught exception: ', err, err.stack);
-});
-
-process.on('SIGINT', function () {
-  try {
-    console.log('Updating reports and shutting down');
-    var ws = new WebSocket('ws://localhost:9001/updateReports?agent=ws');
-    ws.on('close', function () {
-      process.exit();
-    });
-  } catch (e) {
-    process.exit();
-  }
-});
-
-var wss = new WebSocketServer({port: 8181});
-wss.on('connection', function (ws) {
+const wss = new WebSocket.Server({port: 8181});
+wss.on('connection', (ws) => {
   console.log('new connection');
-  ws.on('message', function (data, flags) {
-    ws.send(data, {binary: flags.binary === true});
+  ws.on('message', (data) => {
+    ws.send(data);
   });
-  ws.on('error', function () {
-    console.log('error', arguments);
-  });
+  ws.on('error', (e) => console.error(e));
 });
