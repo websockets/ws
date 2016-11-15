8c73e1da942595d124b773d0196125ac2e22cede
Antti Risteli <antti@kiinnost.us>
2016-11-15 23:16:04 +0200

    Remove useless noise from Autobahn server

--- test/autobahn-server.js
+++ test/autobahn-server.js
@@ -4,7 +4,6 @@ const WebSocket = require('../');
 
 const wss = new WebSocket.Server({port: 8181});
 wss.on('connection', (ws) => {
-  console.log('new connection');
   ws.on('message', (data) => {
     ws.send(data);
   });
