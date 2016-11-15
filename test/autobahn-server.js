de164378892c3dadb55eeec982db5250d30e54b8
Antti Risteli <antti@kiinnost.us>
2016-11-15 23:17:35 +0200

    Autobahn formatting fix

--- test/autobahn-server.js
+++ test/autobahn-server.js
@@ -4,8 +4,6 @@ const WebSocket = require('../');
 
 const wss = new WebSocket.Server({port: 8181});
 wss.on('connection', (ws) => {
-  ws.on('message', (data) => {
-    ws.send(data);
-  });
+  ws.on('message', (data) => ws.send(data));
   ws.on('error', (e) => console.error(e));
 });
