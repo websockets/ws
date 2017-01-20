4d953113a3cb94f6bfac40b62e97184aea2cc243
earthbound19 <ansible1@earthbound.io>
2017-01-20 14:43:28 -0700

    Add comments documenting things that may not be obvious to noobs :)

--- examples/serverstats/server.js
+++ examples/serverstats/server.js
@@ -7,10 +7,14 @@ var server = require('http').createServer();
 app.use(express.static(path.join(__dirname, '/public')));
 
 var wss = new WebSocketServer({server: server});
+  // A page will be served without calling the following control block (and with commenting out the previous code line), but it won't be populated by anything other than what's in the index.html file:
 wss.on('connection', function (ws) {
   var id = setInterval(function () {
     ws.send(JSON.stringify(process.memoryUsage()), function () { /* ignore errors */ });
   }, 100);
+    // For testing, to see in the terminal what that string above is; it's the same as you see in a client browser if you point a browser to localhost:8080 after running this script via 'node server.js'; observe that in index.html, the javascript overwrites values in the HTML document via document.getElementById('elementID').innerHTML = aStringVar; aStringVar being what it is handed here as JSON.stringify(process.memoryUsage().
+    // var logStr = JSON.stringify(process.memoryUsage());
+    // console.log(logStr);
   console.log('started client interval');
   ws.on('close', function () {
     console.log('stopping client interval');
