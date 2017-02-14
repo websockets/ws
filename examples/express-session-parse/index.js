fc8bd0f6a42df262d47fdd7925a5b849324a11ec
Vasily Loginov <loginov.vasily@gmail.com>
2017-02-14 12:41:59 +0700

    Fixed indentation.

--- examples/express-session-parse/index.js
+++ examples/express-session-parse/index.js
@@ -10,9 +10,9 @@ const app = express();
 
 // We will need the same instance of the session parser in express and web socket server.
 const sessionParser = session({
-    secret: '$eCuRiTy',
-    resave: false,
-    saveUninitialized: false
+  secret: '$eCuRiTy',
+  resave: false,
+  saveUninitialized: false
 });
 
 // Give static contents from the 'public' folder for free
@@ -20,63 +20,63 @@ app.use('/', express.static('public'));
 
 app.use(sessionParser);
 app.get('/session', (request, response) => {
-    // "Log in" user and set userId to session
-    const {session} = request;
-    session.userId = uuid.v4();
-    console.log(`New session is created for user ${session.userId}`);
-    response
-        .json({
-            result: 'OK',
-            message: 'User session is created'
-        })
-        .end();
+  // "Log in" user and set userId to session
+  const {session} = request;
+  session.userId = uuid.v4();
+  console.log(`New session is created for user ${session.userId}`);
+  response
+    .json({
+      result: 'OK',
+      message: 'User session is created'
+    })
+    .end();
 });
 
 app.delete('/session', (request, response) => {
-    const {session} = request;
-    if (session.userId) {
-        console.log(`Session is destroying for user ${session.userId}`);
-        request.session.destroy();
-        response
-            .json({
-                result: 'OK',
-                message: 'User session is destroyed'
-            })
-            .end();
-    } else {
-        console.log('Requested to destroy an empty session.');
-        response
-            .json({
-                result: 'error',
-                message: 'No user session'
-            })
-            .end();
-    }
+  const {session} = request;
+  if (session.userId) {
+    console.log(`Session is destroying for user ${session.userId}`);
+    request.session.destroy();
+    response
+      .json({
+        result: 'OK',
+        message: 'User session is destroyed'
+      })
+      .end();
+  } else {
+    console.log('Requested to destroy an empty session.');
+    response
+      .json({
+        result: 'error',
+        message: 'No user session'
+      })
+      .end();
+  }
 });
 
 // Create HTTP server by ourselves
 const httpServer = http.createServer();
 
 var wss = new WebSocketServer({
-    server: httpServer,
-    verifyClient: function (info, done){
-        const request = info.req;
-        console.log('Parsing session from request...');
-        sessionParser(request, {}, () => {
-            console.log('Session is parsed!');
-            const {session} = request;
-            // We can reject the connection by returning false to done(). For example, reject here if user is unknown.
-            done(session && session.userId);
-        });
-    }
+  server: httpServer,
+  verifyClient: function (info, done) {
+    const request = info.req;
+    console.log('Parsing session from request...');
+    sessionParser(request, {}, () => {
+      console.log('Session is parsed!');
+      const {session} = request;
+      // We can reject the connection by returning false to done(). For example, reject here if user is unknown.
+      done(session && session.userId);
+    });
+  }
 });
 
-wss.on('connection', function(ws) {
-    ws.on('message', function(message) {
-        const session = ws.upgradeReq.session;
-        // Here we can now use session parameters.
-        console.log(`WS message ${message} from user ${session.userId}`);
-    });
+wss.on('connection', function (ws) {
+  ws.on('message', function (message) {
+    const session = ws.upgradeReq.session;
+    // Here we can now use session parameters.
+    console.log(`WS message ${message} from user ${session.userId}`);
+  });
 });
 
 // Add express handlers to our HTTP server
@@ -84,7 +84,7 @@ httpServer.on('request', app);
 
 // And start the service at last
 httpServer.listen(SERVICE_PORT, () => {
-    const host = httpServer.address().address;
-    const port = httpServer.address().port;
-    console.log(`Server is listening on ${host}:${port}`);
+  const host = httpServer.address().address;
+  const port = httpServer.address().port;
+  console.log(`Server is listening on ${host}:${port}`);
 });
--- examples/express-session-parse/public/app.js
+++ examples/express-session-parse/public/app.js
@@ -1,42 +1,46 @@
 var resultDiv = document.querySelector('div#result');
 
 function showMessage(message) {
-    resultDiv.innerHTML += '<br/>' + message;
-    resultDiv.scrollTop = resultDiv.scrollHeight;
+  resultDiv.innerHTML += '<br/>' + message;
+  resultDiv.scrollTop = resultDiv.scrollHeight;
 }
 
 function handleResponseAsync(response) {
-    return Promise.resolve()
-        .then(function() {
-            if (response.ok) {
-                response.json()
-                    .then(function(json) {
-                        showMessage(JSON.stringify(json, null, 2))
-                    });
-            } else {
-                showMessage('Network error');
-            }
-        })
+  return Promise.resolve()
+    .then(function () {
+      if (response.ok) {
+        response.json()
+          .then(function (json) {
+            showMessage(JSON.stringify(json, null, 2))
+          });
+      } else {
+        showMessage('Network error');
+      }
+    })
 }
 function openSession() {
-    fetch('/session', {credentials: 'same-origin'})
-        .then(function(response) {handleResponseAsync(response)});
+  fetch('/session', {credentials: 'same-origin'})
+    .then(function (response) {
+      handleResponseAsync(response)
+    });
 }
 function closeSession() {
-    fetch('/session', {method: 'DELETE', credentials: 'same-origin'})
-        .then(function(response) {handleResponseAsync(response)});
+  fetch('/session', {method: 'DELETE', credentials: 'same-origin'})
+    .then(function (response) {
+      handleResponseAsync(response)
+    });
 }
 function connectToWs() {
-    var hostname = window.location.hostname;
-    var port = window.location.port;
-    var Socket = new WebSocket('ws://' + hostname + ':' + port);
-    Socket.onerror = function() {
-        showMessage('Error in web socket');
-    };
-    Socket.onopen = function() {
-        showMessage('Web socket is opened');
-    };
-    Socket.onclose = function() {
-        showMessage('Web socket is closed');
-    }
+  var hostname = window.location.hostname;
+  var port = window.location.port;
+  var Socket = new WebSocket('ws://' + hostname + ':' + port);
+  Socket.onerror = function () {
+    showMessage('Error in web socket');
+  };
+  Socket.onopen = function () {
+    showMessage('Web socket is opened');
+  };
+  Socket.onclose = function () {
+    showMessage('Web socket is closed');
+  }
 }
