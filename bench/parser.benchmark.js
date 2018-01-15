e3660934e8de9891e8b06ce04a6585cc10e1fce4
Quentin Barbe <forty@users.noreply.github.com>
2018-01-15 18:56:25 +0100

    [minor] Remove license comments (#1278)

--- bench/parser.benchmark.js
+++ bench/parser.benchmark.js
@@ -1,9 +1,3 @@
-/*!
- * ws: a node.js websocket client
- * Copyright(c) 2011 Einar Otto Stangvik <einaros@gmail.com>
- * MIT Licensed
- */
-
 'use strict';
 
 const safeBuffer = require('safe-buffer');
