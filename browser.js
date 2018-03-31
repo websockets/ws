4ceef6240892d6c430d12fd20e8cfaa5b94a299d
Huan LI <zixia@zixia.net>
2018-03-31 15:49:06 +0800

    linting

--- browser.js
+++ browser.js
@@ -1,3 +1,3 @@
-module.exports = function() {
-  throw new Error('ws only supports Node.js! Please consider to use `isomorphic-ws` for both Node.js & Browser support! (See issue #1344 at https://github.com/websockets/ws/issues/1344 for more information.')
-}
+module.exports = function () {
+  throw new Error('ws only supports Node.js! Please consider to use `isomorphic-ws` for both Node.js & Browser support! (See issue #1344 at https://github.com/websockets/ws/issues/1344 for more information.');
+};
