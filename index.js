3b5394f5d48590dc332b7010c712952c77e672ad
Edoardo Gallo <edo91.gallo@gmail.com>
2023-02-01 14:04:14 +0100

    export CloseEvent, ErrorEvent, MessageEvent classes

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 'use strict';
 
 const WebSocket = require('./lib/websocket');
+const { CloseEvent, ErrorEvent, MessageEvent } = require('./lib/event-target');
 
 WebSocket.createWebSocketStream = require('./lib/stream');
 WebSocket.Server = require('./lib/websocket-server');
 WebSocket.Receiver = require('./lib/receiver');
 WebSocket.Sender = require('./lib/sender');
+
+WebSocket.CloseEvent = CloseEvent
+WebSocket.ErrorEvent = ErrorEvent
+WebSocket.MessageEvent = MessageEvent
 
 WebSocket.WebSocket = WebSocket;
 WebSocket.WebSocketServer = WebSocket.Server;
--- wrapper.mjs
+++ wrapper.mjs
@@ -3,6 +3,7 @@ import Receiver from './lib/receiver.js';
 import Sender from './lib/sender.js';
 import WebSocket from './lib/websocket.js';
 import WebSocketServer from './lib/websocket-server.js';
+import { CloseEvent, ErrorEvent, MessageEvent } from './lib/event-target.js';
 
-export { createWebSocketStream, Receiver, Sender, WebSocket, WebSocketServer };
+export { createWebSocketStream, Receiver, Sender, WebSocket, WebSocketServer, CloseEvent, ErrorEvent, MessageEvent };
 export default WebSocket;
