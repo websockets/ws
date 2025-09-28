55ffe8c5a5ca76c0338ca9fc5e7100aa6ca138f6
Frank Lemanschik <117739566+lemanschik@users.noreply.github.com>
2025-09-28 09:26:19 +0200

    Update wrapper.mjs
    Fixed: esm warapper which exported nested default

--- wrapper.mjs
+++ wrapper.mjs
@@ -1,8 +1,12 @@
-import createWebSocketStream from './lib/stream.js';
-import Receiver from './lib/receiver.js';
-import Sender from './lib/sender.js';
-import WebSocket from './lib/websocket.js';
-import WebSocketServer from './lib/websocket-server.js';
+// Our SRC is CJS so we need to import default named
+import { default as createWebSocketStream } from './lib/stream.js';
+import { default as Receiver } from './lib/receiver.js';
+import { default as Sender } from './lib/sender.js';
+import { default as WebSocket } from './lib/websocket.js';
+import { default as WebSocketServer } from './lib/websocket-server.js';
 
 export { createWebSocketStream, Receiver, Sender, WebSocket, WebSocketServer };
-export default WebSocket;
+// import * as WS from 'ws' => works
+// import { WebSocket } from 'ws'; => works
+export default { createWebSocketStream, Receiver, Sender, WebSocket, WebSocketServer };
+// import WS from 'ws' => Works
