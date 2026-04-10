af548ff2d6997cbeb01ff95ed6fadf5ad8becdbf
Luciano Alves <luas10c@gmail.com>
2026-04-10 11:21:35 -0300

    [build]: add typescript and configure esm

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,46 @@
-'use strict';
+import { defineConfig, globalIgnores } from 'eslint/config';
+
+import globals from 'globals';
+
+import js from '@eslint/js';
+import ts from 'typescript-eslint';
+
+import prettier from 'eslint-plugin-prettier';
+
+export default defineConfig([
+  globalIgnores([
+    '.nyc_output/',
+    '.vscode/',
+    'coverage/',
+    'dist/',
+    'node_modules/'
+  ]),
+  js.configs.recommended,
+  ts.configs.recommended,
+  {
+    plugins: {
+      prettier
+    },
+    languageOptions: {
+      ecmaVersion: 'latest',
+      globals: {
+        ...globals.browser,
+        ...globals.mocha,
+        ...globals.node
+      },
+      sourceType: 'module'
+    },
+    rules: {
+      ...prettier.configs.recommended.rules,
+      'no-console': 'off',
+      'no-unused-vars': ['error', { caughtErrors: 'none' }],
+      'no-var': 'error',
+      'prefer-const': 'error'
+    }
+  }
+]);
+
+/* 'use strict';
 
 const pluginPrettierRecommended = require('eslint-plugin-prettier/recommended');
 const globals = require('globals');
@@ -7,7 +49,13 @@ const js = require('@eslint/js');
 module.exports = [
   js.configs.recommended,
   {
-    ignores: ['.nyc_output/', '.vscode/', 'coverage/', 'node_modules/'],
+    ignores: [
+      '.nyc_output/',
+      '.vscode/',
+      'coverage/',
+      'dist/',
+      'node_modules/'
+    ],
     languageOptions: {
       ecmaVersion: 'latest',
       globals: {
@@ -26,3 +74,4 @@ module.exports = [
   },
   pluginPrettierRecommended
 ];
+ */
---
+++ prettier.config.js
@@ -0,0 +1,18 @@
+/** @type{import('prettier').Config} */
+export default {
+  singleQuote: true,
+  trailingComma: 'none',
+  bracketSpacing: true,
+  semi: false,
+  printWidth: 80,
+  quoteProps: 'consistent',
+  proseWrap: 'preserve',
+  arrowParens: 'always',
+  bracketSameLine: true,
+  endOfLine: 'lf',
+  singleAttributePerLine: false,
+  jsxSingleQuote: false,
+  useTabs: false,
+  tabWidth: 2,
+  plugins: []
+};
